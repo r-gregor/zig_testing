ae0959466b1379733d690b7184bb27c157fefb38
r-gregor <gredelonghi@gmail.com>
2025-01-13 12:20:03 +0100

    update 20250113_1220_en

---
+++ zt_002_first-test_2_20250113/build.zig
@@ -0,0 +1,12 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const exe = b.addExecutable(.{
+        .name = "3-ziggzagg",
+        .root_source_file = b.path("src/main.zig"),
+        .target = b.standardTargetOptions(.{}),
+        .optimize = b.standardOptimizeOption(.{}),
+    });
+
+    b.installArtifact(exe);
+}
