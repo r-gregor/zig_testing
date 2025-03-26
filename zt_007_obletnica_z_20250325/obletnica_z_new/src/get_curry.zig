90d2e833fbeda629d7a14646cdd1f54c3cf68eee
RgregoR <gredelonghi@gmail.com>
2025-03-26 09:33:35 +0100

    update 20250326_0933_jbe

--- zt_007_obletnica_z_20250325/obletnica_z_new/src/get_curry.zig
+++
@@ -1,9 +0,0 @@
-const time = @import("std").time;
-const epoch = @import("std").time.epoch;
-
-pub fn getCurrentYear() u32 {
-    const now = time.timestamp();
-    const epoch_days: i64 = @divTrunc(now, (24 * 3600));
-    var EpochDays = epoch.EpochDay{ .day = @as(u47, @intCast(epoch_days)) };
-    return EpochDays.calculateYearDay().year; // returns u16
-}
