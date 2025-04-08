819525f1c6e68899fb3bf193e717a823695bc08c
r-gregor <gredelonghi@gmail.com>
2025-04-08 16:46:54 +0200

    update 20250408_1646_mdb

--- zt_007_obletnica_z_20250325/obletnica-z/src/get-curry.zig
+++ zt_007_obletnica_z_20250325/obletnica-z/src/get-curry.zig
@@ -7,6 +7,6 @@ const epoch = time.epoch;
 pub fn getCurrentYear() u32 {
     const now = time.timestamp();
     const epoch_days: i64 = @divTrunc(now, (24 * 3600));
-    var EpochDays = epoch.EpochDay{ .day = @as(u47, @intCast(epoch_days)) };
-    return EpochDays.calculateYearDay().year; // returns u16
+    var epoch_days_struct = epoch.EpochDay{ .day = @as(u47, @intCast(epoch_days)) };
+    return epoch_days_struct.calculateYearDay().year; // returns u16
 }
--- zt_FUNCTIONS/get_curry.zig
+++ zt_FUNCTIONS/get_curry.zig
@@ -1,9 +1,9 @@
 const time = @import("std").time;
-const epoch = @import("std").time.epoch;
+const epoch = time.epoch;
 
 pub fn getCurrentYear() u32 {
     const now = time.timestamp();
     const epoch_days: i64 = @divTrunc(now, (24 * 3600));
-    var EpochDays = epoch.EpochDay{ .day = @as(u47, @intCast(epoch_days)) };
-    return EpochDays.calculateYearDay().year; // returns u16
+    var epoch_days_struct = epoch.EpochDay{ .day = @as(u47, @intCast(epoch_days)) };
+    return epoch_days_struct.calculateYearDay().year; // returns u16
 }
