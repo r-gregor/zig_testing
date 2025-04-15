7ea0399eb24d4e434090089116b72ae9f1536ebf
r-gregor <gredelonghi@gmail.com>
2025-04-15 13:33:01 +0200

    update 20250415_1333_en

--- zt_007_obletnica_z_20250325/obletnica-z/src/main.zig
+++ zt_007_obletnica_z_20250325/obletnica-z/src/main.zig
@@ -2,21 +2,20 @@
 // 20250325 v2 en
 // 20250326 v3 d
 // 20250403 v4 en
+// 20250415 v5 en
+
 const std = @import("std");
 
-const daysInNYear: u32 = 365;
-const daysInLYear: u32 = 366;
-const secondsInNYear: u32 = daysInNYear * 24 * 3600;
-const secondsInLYear: u32 = daysInLYear * 24 * 3600;
+// local imports
+const currYr = @import("get-curry.zig");
+const leapYr = @import("is-leap-year.zig");
+
+const daysInYear: u32 = 365;
 
 fn crtc() !void {
     const out = std.io.getStdOut().writer();
     try out.print("{s}\n", .{"-" ** 50});
 }
-
-// local imports
-const currYr = @import("get-curry.zig");
-const leapYr = @import("is-leap-year.zig");
 
 pub fn main() !void {
     const startYear: u64 = 1989;
@@ -43,10 +42,10 @@ pub fn main() !void {
     var leapYears: u64 = 0;
     for (0..yearsBetween) |i| {
         if (leapYr.isLeapYear(@as(u64, startYear) + i)) {
-            loveDays += daysInLYear;
+            loveDays += daysInYear + 1;
             leapYears += 1;
         } else {
-            loveDays += daysInNYear;
+            loveDays += daysInYear;
         }
     }
 
